31d31f1fb07dd28fd4917710a83b672c432709b5
Hallboo <zhanghaobo@mipitalk.com>
2021-05-08 22:16:28 +0800

    Implement Worker2 based on LPCNet python API

--- server/worker2.py
+++ server/worker2.py
@@ -7,6 +7,8 @@
 import os
 import re
 import sys
+import wave
+import time
 
 import yaml
 import logging
@@ -27,50 +29,82 @@ from espnet.asr.asr_utils import get_model_conf
 from espnet.asr.asr_utils import torch_load
 from espnet.utils.dynamic_import import dynamic_import
 
+import lpcnet 
+import frontend
+
 with open('config/config.yaml', 'r', encoding = 'utf-8') as fp:
     config = yaml.safe_load(fp)
 
 device = torch.device(config['torch_device'])
 
-def RunLPCNet(overlap, num_chunk_frame, vocoder_path, tmp_dir, sub_f32_path, req_id, sub_req_id, s1, s2):
+def run_vocoder(overlap, num_chunk_frame, tmp_dir, i, s1, s2, a1, a2, sub_feats):
 
-    if s2 == s1 + overlap + num_chunk_frame + overlap:
-        duration = num_chunk_frame
-    else:
-        duration = (s2 - s1 - overlap)
+    pcm = lpcnet.run_for_chunk(sub_feats.tolist())
+    pcm = np.array(pcm, dtype = "int16")
+
+    duration = a2 - a1
 
     if s1 != 0:
         start = overlap
     else:
         start = 0
 
-    #print("s1:{} s2:{} start:{} Duration:{}".format(s1, s2, s1+start, s1+start+duration))
+    logging.debug("{} LPCNet API {:2d} {:4d} {:4d} {:4d} {:4d} shape:{} start {:2d} dur {:2d}".format(
+        __file__, i, s1, s2, a1, a2, pcm.shape, start, duration))
 
-    start = start * 0.01
-    duration = duration * 0.01
+    return (i, pcm[start:start+duration])
 
-    sub_pcm_path = os.path.join(tmp_dir, req_id, sub_req_id + "-wave.pcm")
-    sub_wav_path = os.path.join(tmp_dir, req_id, sub_req_id + "-wave.wav")
-    os.system("{} -synthesis {} {}".format(vocoder_path, sub_f32_path, sub_pcm_path))
+def Split(overlap, npy_feat, num_chunk_frame):
 
-    os.system("sox -c 1 -r 16000 -t sw {} -t wav {} trim {} {}".format(
-            sub_pcm_path, sub_wav_path, start, duration))
+    features = np.zeros((npy_feat.shape[0], 55), dtype='float32')
 
-    return sub_wav_path
+    features[ : , :18 ]   = npy_feat[ :, :18 ]
+    features[ : , 36:38 ] = npy_feat[ :, 18: ]
 
-def CombineAudio(wav_batch_path, wav_path):
-    wav_batch_path.sort()
-    waves = " ".join(wav_batch_path)
-    os.system("sox {} {}".format(waves, wav_path))
+    sub_range = int(features.shape[0]/num_chunk_frame) + 1
+
+    logging.debug("{} num_frame:{} sub_range:{}".format(
+        __file__, features.shape[0], sub_range))
+
+    # [ seg1(n1,55), seg2(n2,55), ... seg(N, 55) ]
+    batch_feats = []
+    for i in range(sub_range):
+        
+        a1 = i * num_chunk_frame
+
+        a2 = i * num_chunk_frame + num_chunk_frame
+        if a2 > features.shape[0]:
+            a2 = features.shape[0]
+
+        if a2 - a1 < overlap: break
+
+        s1, s2 = a1, a2
+
+        if s2 < features.shape[0]:
+            s2 = s2 + overlap
+            if s2 > features.shape[0]:
+                s2 = features.shape[0]
+
+        if i != 0: s1 = s1 - overlap
+
+        logging.debug("{} Split [{}] {:4d} {:4d} {:4d} {:4d}".format(__file__, i, 
+                        s1, s2, a1, a2))
+
+        segment_feats = features[s1:s2]
+        batch_feats.append([i, s1, s2, a1, a2, segment_feats])
+
+    return batch_feats
 
 class TacotronLPCNetWorker():
     def __init__(self, config):
+
+        logging.info("Worker2 init start:")
 
         self.tmp_dir = config['tmp']
         #os.makedirs(self.tmp_dir, exist_ok=True)
         self.debug_mode = config['debug_mode']
 
-        self.phone2id = self.LoadDictionary(config['dict_path'])
+        self.phone2id = frontend.LoadDictionary(config['dict_path'])
 
         self.idim, odim, train_args = get_model_conf(config['acoustic_model_path'])
 
@@ -94,161 +128,94 @@ class TacotronLPCNetWorker():
         self.overlap = config["overlap"]       
         logging.info("Overlap: {}".format(self.overlap))
 
-        self.vocoder_path = config['vocoder_path']
-        logging.info("Vocoder Path: {}".format(self.vocoder_path))
-
-    def SaveAsF32(self, npy_feat, f32_path):
-
-        features = np.zeros((npy_feat.shape[0], 55), dtype='float32')
-        features[ : , :18 ]   = npy_feat[ :, :18 ]
-        features[ : , 36:38 ] = npy_feat[ :, 18: ]
-
-        with open(f32_path, 'wb') as fp:
-            features.tofile(fp)
-        logging.info("{} Save file: {}".format(__file__, f32_path))
-
-    def SaveNPartAsF32(self, overlap, npy_feat, num_chunk_frame, req_dir, req_id):
-
-        features = np.zeros((npy_feat.shape[0], 55), dtype='float32')
-        features[ : , :18 ]   = npy_feat[ :, :18 ]
-        features[ : , 36:38 ] = npy_feat[ :, 18: ]
-
-        sub_range = int(features.shape[0]/num_chunk_frame) + 1
-        #print(features.shape[0], sub_range)
-
-        f32_batch_path = []
-        for i in range(sub_range):
-            
-            s1 = i * num_chunk_frame
-
-            s2 = i * num_chunk_frame + num_chunk_frame
-            if s2 > features.shape[0]:
-                s2 = features.shape[0]
-
-            if s2 - s1 < overlap: break
-
-            #print("A s1:{} s2:{}".format(s1, s2))
-
-            if s2 < features.shape[0]:
-                s2 = s2 + overlap
-                if s2 > features.shape[0]:
-                    s2 = features.shape[0]
-
-            if i != 0: s1 = s1 - overlap
-
-            #print("B s1:{} s2:{}".format(s1, s2))
- 
-            surfix = str(i).zfill(3)
-
-            sub_req_id = "{}-{:05d}-{:05d}".format(req_id, s1,s2)
-            f32_name = "{}-feats.f32".format(sub_req_id)
-            f32_path = os.path.join(req_dir, f32_name)
-            #print(f32_path)
-
-            with open(f32_path, 'wb') as fp:
-                part_features = features[s1:s2]
-                part_features.tofile(fp)
-                #logging.info("{} Save file: {}".format(__file__, f32_path))
-
-                f32_batch_path.append([sub_req_id, f32_path, s1, s2])
-
-        return f32_batch_path
-
-    # def RunLPCNet(self, sub_f32_path, req_id, sub_req_id):
-
-    #     sub_pcm_path = os.path.join(self.tmp_dir, req_id, sub_req_id + "-wave.pcm")
-    #     sub_wav_path = os.path.join(self.tmp_dir, req_id, sub_req_id + "-wave.wav")
-
-    #     os.system("{} {} {}".format(self.vocoder_path, sub_f32_path, sub_pcm_path))
-    #     logging.info("{} Save file: {}".format(__file__, sub_pcm_path))
-    #     os.system("sox -c 1 -r 16000 -t sw {} -t wav {}".format(sub_pcm_path, sub_wav_path))
-    #     logging.info("{} Save file: {}".format(__file__, sub_wav_path))
-
-    #     return sub_wav_path
-
     def Text2Speech(self, text, req_id):
-        mat = self.Frontend(text)
+
+        time_start = time.time()
+
+        # 1. front end
+        idseq = frontend.Frontend(self.phone2id, self.idim, text)
+        mat = torch.LongTensor(idseq).view(-1).to(device)
+        time_frontend = time.time()
+
+        # 2. acoustic model inference
         c, _, _ = self.model.inference(mat, self.inference_args)
-        f=c.cpu().detach().numpy()
+        features = c.cpu().detach().numpy()
 
         logging.info("{} Pass Acoustic Model".format(__file__))
+        time_acoustic = time.time()
 
         req_dir = os.path.join(self.tmp_dir, req_id)
 
         os.makedirs(req_dir, exist_ok=True)
 
-        f32_batch_path = self.SaveNPartAsF32(self.overlap, f, self.num_chunk_frame, req_dir, req_id)
-        logging.info("{} Save F32 Done".format(__file__))
+        # 3. vocoder
+        batch_feats = Split(self.overlap, features, self.num_chunk_frame)
+        logging.info("{} Split {} done".format(__file__, len(batch_feats)))
 
-        executor = ProcessPoolExecutor(max_workers=len(f32_batch_path))
+        executor = ProcessPoolExecutor(max_workers=len(batch_feats))
         futures = []
 
-        for sub_req_id, sub_f32_path, s1, s2 in f32_batch_path:
+        num_frame = 0
+        for i, s1, s2, a1, a2, sub_feats in batch_feats:
+            num_frame += a2 - a1
             futures.append(executor.submit(
-                partial(RunLPCNet, self.overlap, self.num_chunk_frame, self.vocoder_path,
-                self.tmp_dir, sub_f32_path, req_id, sub_req_id, s1, s2)
+                partial(run_vocoder, self.overlap, self.num_chunk_frame, self.tmp_dir,
+                        i, s1, s2, a1, a2, sub_feats)
             ))
 
-        wav_batch_path = [ future.result() for future in futures ]
+        batch_pcm = [ future.result() for future in futures ]
+
+        pcm = np.zeros((num_frame, 160), dtype = "int16")
+
+        index = 0
+        for one_pcm in batch_pcm:
+            i, sub_pcm = one_pcm
+            #print(i, sub_pcm.shape)            
+            start = index 
+            num   = sub_pcm.shape[0]
+            #print(start, num)
+            pcm[start: start + num ]  = sub_pcm
+            index += num
+
+        pcm = np.reshape(pcm, -1 )[: num_frame * 160 ]
 
         wav_path = os.path.join(req_dir, req_id + "-wave.wav")
-        CombineAudio(wav_batch_path, wav_path)
 
-        logging.info("{} Wave {}".format(__file__, wav_path))
+        with wave.open(wav_path, 'wb') as fp:
+            # 1 channel, 16 bits, 16000 sample rate
+            fp.setparams((1, 2, 16000, 0, 'NONE', 'NONE'))
+            fp.writeframes(pcm)
+
+        audio_dur = num_frame * 0.01
+        logging.info("{} Wave {:0.3f}s {}".format(__file__, audio_dur, wav_path))
 
         if self.debug_mode:
             # numpy type feature
-            np.save(os.path.join(req_dir, req_id + "-feats.npy"), f)
+            np.save(os.path.join(req_dir, req_id + "-feats.npy"), features)
             # plot
             plt.figure()
-            plt.matshow(np.flip(f.T))
+            plt.matshow(np.flip(features.T))
             plt.savefig(os.path.join(req_dir, req_id + "-demo.png"), format="png")
             plt.close()
 
+        time_vocoder = time.time()
+
+        time_count_frontend = time_frontend - time_start
+        time_count_acoustic = time_acoustic - time_frontend
+        time_count_vocoder  = time_vocoder  - time_acoustic
+
+        logging.info("{} Acoustic:{:0.3f}s Vocoder:{:0.3f}s A+V: {:0.3f}s".format(
+            __file__, time_count_acoustic, time_count_vocoder, time_vocoder - time_start))
+
         return wav_path
 
-    def Frontend(self, text):
-        """Clean text and then convert to id sequence."""
-        idseq = []
-        T=re.split(r"",text)
-
-        for w in T:
-            if w == ' ':
-                w = "<space>"
-
-            if len(w) == 0:
-                continue
-            
-            if w not in self.phone2id.keys():
-                warnings.warn('%s => %s'%(w,w.lower()))
-                w = w.lower()
-                assert(w in phone2id.keys())
-                idseq += [self.phone2id[w]]
-            else:
-                idseq += [self.phone2id[w]]
-
-        idseq += [ self.idim - 1 ]  # <eos>
-
-        return torch.LongTensor(idseq).view(-1).to(device)
-
-    def LoadDictionary(self, dict_path):
-
-        logging.info("Load Dictionary: {}".format(dict_path))
-
-        with open(dict_path) as fp:
-            lines = fp.readlines()
-            lines = [ line.replace("\n", "").split(" ") for line in lines ]
-
-        phone2id = { c: int(i) for c, i in lines }
-
-        logging.info('Dictionary Size: {}'.format(len(phone2id)))
-
-        return phone2id
 
 if __name__ == "__main__":
 
     # code for test
-    logging.basicConfig(level=logging.DEBUG, format="%(levelname)8s %(asctime)s %(message)s ")
+    # log_level = logg.DEBUG
+    log_level = logging.INFO
+    logging.basicConfig(level=log_level, format="%(levelname)8s %(asctime)s %(message)s ")
     logging.info('Starting Up Tacotron LPCNet Worker')
 
     with open('config/config.yaml', 'r', encoding = 'utf-8') as fp:
